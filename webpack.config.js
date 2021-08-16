84a3e872cf18feb05ec8a0a5c05c5c5829dacdbf
Qvanjian <zqaptx@163.com>
2021-08-16 12:01:10 +0800

    add DefinePlugin for vye global variables

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,14 @@
 /*
  * @Author: Qvanjian
  * @Date: 2021-08-14 11:53:46
- * @LastEditTime: 2021-08-15 18:34:08
+ * @LastEditTime: 2021-08-16 11:58:38
  * @LastEditors: Qvanjian
  * @Description: Basic webpack config for both development and production
  * @FilePath: \vue3-webpack-template\webpack.config.js
  */
 "use strict";
 
+const webpack = require("webpack");
 const { resolve } = require("path");
 const { VueLoaderPlugin } = require("vue-loader");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
@@ -30,21 +31,21 @@ module.exports = {
               outputPath: resolve(__dirname, "dist/pics"),
             },
           },
-        ],        
+        ],
       },
       {
         test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
         use: {
-          loader: 'file-loader'
-        }
+          loader: "file-loader",
+        },
       },
       {
         test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/,
-        loader: 'url-loader',
+        loader: "url-loader",
         options: {
           limit: 40000,
-          name: 'media/[name].[hash:7].[ext]'
-        }
+          name: "media/[name].[hash:7].[ext]",
+        },
       },
       {
         test: /\.vue$/,
@@ -53,14 +54,19 @@ module.exports = {
       {
         test: /\.js$/,
         loader: "babel-loader",
-        exclude: file => (!/\.vue\.js/.test(file)),
+        exclude: (file) => !/\.vue\.js/.test(file),
       },
     ],
   },
   plugins: [
-    new VueLoaderPlugin(), 
+    new VueLoaderPlugin(),
     new HtmlWebpackPlugin({
       template: "./src/index.html",
       filename: "index.html",
-    }),],
+    }),
+    new webpack.DefinePlugin({
+      __VUE_OPTIONS_API__: JSON.stringify(true),
+      __VUE_PROD_DEVTOOLS__: JSON.stringify(false),
+    }),
+  ],
 };
