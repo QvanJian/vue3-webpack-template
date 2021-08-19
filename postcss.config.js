c3ab677a68ef04575fe8e22bf9e3f51044863fdc
Qvanjian <zqaptx@163.com>
2021-08-19 13:26:03 +0800

    remove useless module

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,7 @@
 /*
  * @Author: Qvanjian
  * @Date: 2021-08-14 13:51:43
- * @LastEditTime: 2021-08-15 16:28:01
+ * @LastEditTime: 2021-08-19 13:14:47
  * @LastEditors: Qvanjian
  * @Description: Config for postcss
  * @FilePath: \vue3-webpack-template\postcss.config.js
@@ -12,9 +12,5 @@ module.exports = {
   plugins: [
     //自动前缀
     require("autoprefixer"),
-    //添加svg的css语法修改
-    require("postcss-inline-svg"),
-    //删除冗余svg代码
-    require("postcss-svgo"),
   ],
 };
